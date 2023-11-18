d7e0903020d1a33af17eb4bdb5d08be69d62397a
Colton Skiera <cskiera142@gmail.com>
2023-11-18 07:50:58 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+import { data } from "./jsonData.js";
+const tbody = document.querySelector("#stockTableContent");
+
+console.log(data);
+
+const stock = [];
+window.addEventListener("load", (e) => {
+  setInterval(addRecord, 3000);
+});
+
+//
+//
+function randRange(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function addRecord() {
+  const randomInt = randRange(0, 18);
+  const randomVariation = randRange(-100, 100);
+  const stockEntry = {
+    ...data[randomInt],
+    variation: `${randomVariation}K`,
+  };
+  stock.push(stockEntry);
+  draw();
+}
+
+function draw() {
+  console.log(stock);
+  tbody.innerHTML = "";
+
+  for (let i = 1; i <= stock.length; i++) {
+    const stockTickerData = stock[i];
+
+    let tr = document.createElement("tr");
+    for (const propName in stockTickerData) {
+      let td = document.createElement("td");
+      td.textContent = stockTickerData[propName];
+      tr.append(td);
+    }
+    tbody.append(tr);
+  }
+}
